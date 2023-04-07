11670a639058e708e3a1a578c524458fc7effdc4
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-04-07 17:43:00 +0200

    changed the benchmarking distributions

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -1,4 +1,6 @@
 use webgraph::codes::*;
+use rand::prelude::*;
+use rand::distributions::Distribution;
 use core::arch::x86_64::{_rdtsc, __rdtscp, __cpuid, _mm_lfence, _mm_mfence, _mm_sfence};
 
 const VALUES: usize = 10_000;
@@ -7,18 +9,6 @@ const BENCH_ITERS: usize = 10_000;
 const CALIBRATION_ITERS: usize = 1_000_000;
 const SEED: u64 = 0x8c2b_781f_2866_90fd;
 const TSC_FREQ: u64 = 4_000_000_000;  
-
-struct Rng(u64);
-impl Rng {
-    fn next(&mut self) -> u64 {
-        let mut x = self.0;
-        x ^= x << 13;
-        x ^= x >> 7;
-        x ^= x << 17;
-        self.0 = x;
-        x
-    }
-}
 
 fn rdtsc() -> u64 {
     unsafe{
@@ -61,13 +51,7 @@ fn pin_to_core(core_id: usize) {
 }
 
 macro_rules! bench {
-    ($cal:expr, $mod_name:literal, $reader:ident, $writer:ident, $code:literal, $read:ident, $write:ident, $bo:ident, $table:expr) => {{
-let mut rng = Rng(SEED);
-let random_vals = (0..VALUES)
-    .map(|_| {
-        rng.next() % (1 + rng.next() % 1000)
-    })
-    .collect::<Vec<_>>();
+    ($cal:expr, $mod_name:literal, $reader:ident, $writer:ident, $code:literal, $read:ident, $write:ident, $data:expr, $bo:ident, $table:expr) => {{
 let mut buffer = Vec::with_capacity(VALUES);
 let mut read_time: u64 = 0;
 let mut write_time: u64 = 0;
@@ -78,7 +62,7 @@ for iter in 0..(WARMUP_ITERS + BENCH_ITERS) {
             MemWordWriteVec::new(&mut buffer)
         );
         let w_start = rdtsc();
-        for value in &random_vals {
+        for value in &$data {
             r.$write::<$table>(*value).unwrap();
         }
         let w_end = rdtsc();
@@ -91,7 +75,7 @@ for iter in 0..(WARMUP_ITERS + BENCH_ITERS) {
             MemWordRead::new(&mut buffer)
         );
         let r_start = rdtsc();
-        for _ in &random_vals {
+        for _ in &$data {
             r.$read::<$table>().unwrap();
         }
         let r_end = rdtsc();
@@ -124,29 +108,45 @@ println!("{}::{}::{}::{},{},{},{},{},{},{}",
 }
 
 macro_rules! impl_code {
-    ($cal:expr, $mod_name:literal, $reader:ident, $writer:ident, $code:literal, $read:ident, $write:ident) => {
+    ($cal:expr, $mod_name:literal, $reader:ident, $writer:ident, $code:literal, $read:ident, $write:ident, $data:expr) => {
         bench!(
-            $cal, $mod_name, $reader, $writer, $code, $read, $write, M2L, false
+            $cal, $mod_name, $reader, $writer, $code, $read, $write, $data, M2L, false
         );
         bench!(
-            $cal, $mod_name, $reader, $writer, $code, $read, $write, M2L, true
+            $cal, $mod_name, $reader, $writer, $code, $read, $write, $data, M2L, true
         );
         bench!(
-            $cal, $mod_name, $reader, $writer, $code, $read, $write, L2M, false
+            $cal, $mod_name, $reader, $writer, $code, $read, $write, $data, L2M, false
         );
         bench!(
-            $cal, $mod_name, $reader, $writer, $code, $read, $write, L2M, true
+            $cal, $mod_name, $reader, $writer, $code, $read, $write, $data, L2M, true
         );
     };
 }
 
 macro_rules! impl_bench {
     ($cal:expr, $mod_name:literal, $reader:ident, $writer:ident) => {
+        let mut rng = rand::thread_rng();
+        
+        let unary_data = (0..VALUES)
+            .map(|_| {
+                let v: u64 = rng.gen();
+                v.trailing_zeros() as u64
+            })
+            .collect::<Vec<_>>();
+
         impl_code!(
-            $cal, $mod_name, $reader, $writer, "unary", read_unary, write_unary
+            $cal, $mod_name, $reader, $writer, "unary", read_unary, write_unary, unary_data
         );
+
+        let mut zipf = zipf::ZipfDistribution::new(1000, 2.0).unwrap();
+        let gamma_data = (0..VALUES)
+            .map(|_| {
+                zipf.sample(&mut rng) as u64
+            })
+            .collect::<Vec<_>>();
         impl_code!(
-            $cal, $mod_name, $reader, $writer, "gamma", read_gamma, write_gamma
+            $cal, $mod_name, $reader, $writer, "gamma", read_gamma, write_gamma, gamma_data
         );
     };
 }
