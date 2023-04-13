62aea1001c28dd51e10316617f574316b5d24204
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-04-13 16:14:57 +0200

    added regeneration of tables if any table file misses

--- build.rs
+++ build.rs
@@ -1,9 +1,14 @@
 use std::path::PathBuf;
 
 fn main() {
-    let path: PathBuf = ["src", "codes", "unary_tables.rs"].iter().collect();
+    let paths: Vec<PathBuf> = vec![
+        ["src", "codes", "unary_tables.rs"].iter().collect(),
+        ["src", "codes", "gamma_tables.rs"].iter().collect(),
+        ["src", "codes", "delta_tables.rs"].iter().collect(),
+        ["src", "codes", "zeta_tables.rs"].iter().collect(),
+    ];
     // generate the tables if needed
-    if !path.exists() {
+    if paths.iter().any(|path| !path.exists()) {
         std::process::Command::new("python")
             .arg("./code_tables_generator.py")
             .spawn()
