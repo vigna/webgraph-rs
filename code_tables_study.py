c768a3dce34c40087cfe2d5c698e6e20ce8e7fe2
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-04-13 13:42:36 +0200

    reverted stdout change

--- code_tables_study.py
+++ code_tables_study.py
@@ -6,27 +6,33 @@ import os
 import subprocess
 from code_tables_generator import *
 
-for bits in range(2, 18):
-    print(bits)
-    # Create the tables
-    gen_unary(bits, 63)
-    gen_gamma(bits, 256)
-    gen_delta(bits, 256)
-    
-    # Run the benchmark with native cpu optimizations
-    stdout = subprocess.check_output(
-        "cargo run --release", shell=True,
-        env={
-            **os.environ,
-            "RUSTFLAGS":"-C target-cpu=native",
-        },
-        cwd="benchmarks",
-    ).decode()
+with open("tables.csv", "w") as f:
+    for bits in range(1, 18):
+        print(bits)
+        # Create the tables
+        gen_unary(bits, 63)
+        gen_gamma(bits, 256)
+        gen_delta(bits, 256)
+        
+        # Run the benchmark with native cpu optimizations
+        stdout = subprocess.check_output(
+            "cargo run --release", shell=True,
+            env={
+                **os.environ,
+                "RUSTFLAGS":"-C target-cpu=native",
+            },
+            cwd="benchmarks",
+        ).decode()
 
-    # Dump the header only the first time
-    if bits == 1:
-        print("n_bits," + stdout.split('\n')[0])
-    # Dump all lines and add the `n_bits` column
-    for line in stdout.split("\n")[1:]:
-        if len(line.strip()) != 0:
-            print("{},{}".format(bits, line))
+        # Dump the header only the first time
+        if bits == 1:
+            f.write("n_bits,")
+            f.write(stdout.split('\n')[0])
+            f.write("\n")
+        # Dump all lines and add the `n_bits` column
+        for line in stdout.split("\n")[1:]:
+            if len(line.strip()) != 0:
+                f.write("{},".format(bits))
+                f.write(line)
+                f.write("\n")
+        f.flush()
