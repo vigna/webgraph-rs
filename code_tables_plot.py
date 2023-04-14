fde448f404878672fb050050c8c1a9489f7aa09a
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-04-14 16:54:26 +0200

    fixed the plotting of sep and merged tables

--- code_tables_plot.py
+++ code_tables_plot.py
@@ -9,30 +9,44 @@ df = pd.read_csv(sys.stdin, index_col=None, header=0)
 
 for code in ["unary", "gamma", "delta", "zeta3"]:
     plt.figure(figsize=(10, 8), dpi=200, facecolor="white")
-    for tables_n in [1, 2]:
-        if tables_n == 1:
-            table_txt = "merged"
-            marker = "o"
-        else:
-            table_txt = "sep"
-            marker = "s"
-        for ty in ["read_buff", "read_unbuff"]:
+    for ty in ["read_buff", "read_unbuff"]:
+        for tables_n in [1, 2]:
+            if tables_n == 1:
+                table_txt = "merged"
+                marker = "o"
+            else:
+                table_txt = "sep"
+                marker = "s"
+
             for pat in [
                 "%s::L2M::Table"%code,
                 "%s::M2L::Table"%code,
-                "%s::L2M::NoTable"%code,
-                "%s::M2L::NoTable"%code,
             ]:
                 values = df[(df.pat == pat) & (df.type == ty) & (df.tables_num == tables_n)]
                 plt.errorbar(
                     values.n_bits, values.ns_median, values.ns_std,
-                    label="{}::{}::{}_median_std".format(pat, table_txt, ty), 
+                    label="{}::{}::{}".format("::".join(pat.split("::")[1:]), table_txt, ty), 
                     marker=marker,
                 )
                 plt.fill_between(
                     values.n_bits, values.ns_perc25, values.ns_perc75, 
                     alpha=0.3,
                 )
+
+        for pat in [
+            "%s::L2M::NoTable"%code,
+            "%s::M2L::NoTable"%code,
+        ]:  
+            values = df[(df.pat == pat) & (df.type == ty)].groupby("n_bits").mean()
+            plt.errorbar(
+                values.index, values.ns_median, values.ns_std,
+                label="{}::{}".format("::".join(pat.split("::")[1:]), ty), 
+                marker="^",
+            )
+            plt.fill_between(
+                values.index, values.ns_perc25, values.ns_perc75, 
+                alpha=0.3,
+            )
 
     ratios = df[df.pat.str.contains(code) & (df.tables_num == tables_n)].groupby("n_bits").mean()
     bars = plt.bar(
@@ -69,7 +83,7 @@ for code in ["unary", "gamma", "delta", "zeta3"]:
     plt.ylim(bottom=0) #ymin is your value
     plt.xlim([left, right]) #ymin is your value
     plt.xticks(ratios.index)
-    plt.title("Performances of %s codes read and writes\nin function of the table size\nShaded areas are the 25%% and 75%% percentiles."%(code.capitalize()))
+    plt.title("Performances of %s codes read and writes\nin function of the table size\nShaded areas are the 25%% and 75%% percentiles and the plots are medians with stds."%(code.capitalize()))
     plt.xlabel("Table Bits")
     plt.ylabel("ns")
     plt.savefig("%s_tables.png"%code, bbox_inches="tight")
