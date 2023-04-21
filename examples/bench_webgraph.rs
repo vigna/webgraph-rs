3eb4dd00d0897d9b0f17a212cb2f09ea9d3f80a9
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-04-21 19:13:44 +0200

    More parameterized Webgraph speed tests

--- examples/bench_webgraph.rs
+++ examples/bench_webgraph.rs
@@ -1,15 +1,48 @@
+use clap::Parser;
+use java_properties;
+use rand::rngs::SmallRng;
+use rand::Rng;
+use rand::SeedableRng;
+use std::fs::File;
+use std::io::BufReader;
 use webgraph::prelude::*;
 
 type ReadType = u32;
 type BufferType = u64;
 
-const NODES: usize = 325_557;
-const ARCS: usize = 3_216_152;
-const PATH: &str = "tests/data/cnr-2000";
+#[derive(Parser, Debug)]
+#[command(about = "Benchmarks the Rust Webgraph implementation", long_about = None)]
+struct Args {
+    /// The basename of the graph.
+    basename: String,
 
-pub fn main() {
+    /// The number of test repetitions
+    #[arg(short, long, default_value = "10")]
+    repeats: usize,
+
+    /// The number of successor lists in random-access tests
+    #[arg(short, long, default_value = "1000000")]
+    n: u64,
+
+    /// Test sequential access speed by scanning the whole graph
+    #[arg(short = 's', long)]
+    sequential: bool,
+
+    /// Do not test speed, but check that the sequential and random-access successor lists are the same
+    #[arg(short = 'c', long)]
+    check: bool,
+}
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let f = File::open(format!("{}.properties", args.basename))?;
+    let map = java_properties::read(BufReader::new(f))?;
+
+    let num_nodes = map.get("nodes").unwrap().parse::<u64>()?;
+    let num_arcs = map.get("arcs").unwrap().parse::<u64>()?;
     // Read the offsets
-    let mut data = std::fs::read(format!("{}.offsets", PATH)).unwrap();
+    let mut data = std::fs::read(format!("{}.offsets", args.basename)).unwrap();
     // pad with zeros so we can read with ReadType words
     while data.len() % core::mem::size_of::<ReadType>() != 0 {
         data.push(0);
@@ -21,15 +54,16 @@ pub fn main() {
         .collect::<Vec<_>>();
 
     // Read the offsets gammas
-    let mut offsets = Vec::with_capacity(NODES);
-    let mut reader = BufferedBitStreamRead::<M2L, BufferType, _>::new(MemWordReadInfinite::new(&data));
+    let mut offsets = Vec::with_capacity(num_nodes as usize);
+    let mut reader =
+        BufferedBitStreamRead::<M2L, BufferType, _>::new(MemWordReadInfinite::new(&data));
     let mut offset = 0;
-    for _ in 0..NODES {
+    for _ in 0..num_nodes {
         offset += reader.read_gamma::<true>().unwrap() as usize;
         offsets.push(offset);
     }
 
-    let mut data = std::fs::read(format!("{}.graph", PATH)).unwrap();
+    let mut data = std::fs::read(format!("{}.graph", args.basename)).unwrap();
     // pad with zeros so we can read with ReadType words
     while data.len() % core::mem::size_of::<ReadType>() != 0 {
         data.push(0);
@@ -40,30 +74,74 @@ pub fn main() {
         .map(|chunk| ReadType::from_ne_bytes(chunk.try_into().unwrap()))
         .collect::<Vec<_>>();
 
-    // create a random access reader
-    let code_reader = DefaultCodesReader::new(
-        BufferedBitStreamRead::<M2L, BufferType, _>::new(MemWordReadInfinite::new(&data)),
-    );
-    let random_reader = WebgraphReaderRandomAccess::new(code_reader, offsets, 4);
+    if args.check {
+        // Create a sequential reader
+        let mut code_reader = DefaultCodesReader::new(
+            BufferedBitStreamRead::<M2L, BufferType, _>::new(MemWordReadInfinite::new(&data)),
+        );
+        let mut seq_reader = WebgraphReaderSequential::new(&mut code_reader, 4, 16);
 
-    // Create a sequential reader
-    let mut code_reader = DefaultCodesReader::new(
-        BufferedBitStreamRead::<M2L, BufferType, _>::new(MemWordReadInfinite::new(&data)),
-    );
-    let mut seq_reader = WebgraphReaderSequential::new(&mut code_reader, 4, 16);
+        // create a random access reader
+        let code_reader = DefaultCodesReader::new(
+            BufferedBitStreamRead::<M2L, BufferType, _>::new(MemWordReadInfinite::new(&data)),
+        );
+        let random_reader = WebgraphReaderRandomAccess::new(code_reader, offsets, 4);
 
-    // Check that they read the same
-    let start = std::time::Instant::now();
-    for node_id in 0..(NODES as u64) {
-        let _ = seq_reader.get_successors_iter(node_id).unwrap();
+        // Check that sequential and random-access interfaces return the same result
+        for node_id in 0..num_nodes {
+            let seq = seq_reader.get_successors_iter(node_id)?;
+            let random = random_reader
+                .get_successors_iter(node_id)?
+                .collect::<Vec<_>>();
+
+            // Why won't assert!(seq.iter().eq(random.iter())) work if I don't collect?
+            assert!(seq.iter().eq(random.iter()));
+        }
+    } else if args.sequential {
+        // Sequential speed test
+        for _ in 0..args.repeats {
+            // Create a sequential reader
+            let mut code_reader =
+                DefaultCodesReader::new(BufferedBitStreamRead::<M2L, BufferType, _>::new(
+                    MemWordReadInfinite::new(&data),
+                ));
+            let mut seq_reader = WebgraphReaderSequential::new(&mut code_reader, 4, 16);
+            let mut c: usize = 0;
+            let start = std::time::Instant::now();
+            for node_id in 0..num_nodes {
+                c += seq_reader.get_successors_iter(node_id)?.iter().count();
+            }
+            println!(
+                "Sequential:{:>20} ns/arcs",
+                (start.elapsed().as_secs_f64() / c as f64) * 1e9
+            );
+
+            assert_eq!(c, num_arcs as usize);
+        }
+    } else {
+        // Random-access speed test
+        for _ in 0..args.repeats {
+            // create a random access reader
+            let code_reader = DefaultCodesReader::new(
+                BufferedBitStreamRead::<M2L, BufferType, _>::new(MemWordReadInfinite::new(&data)),
+            );
+            let random_reader = WebgraphReaderRandomAccess::new(code_reader, offsets.clone(), 4);
+
+            let mut random = SmallRng::seed_from_u64(0);
+            let mut c: usize = 0;
+
+            let start = std::time::Instant::now();
+            for _ in 0..args.n {
+                c += random_reader
+                    .get_successors_iter(random.gen_range(0..args.n))?
+                    .count();
+            }
+
+            println!(
+                "Random:    {:>20} ns/arcs",
+                (start.elapsed().as_secs_f64() / c as f64) * 1e9
+            );
+        }
     }
-    println!("Sequential:{:>20} ns/arcs", (start.elapsed().as_secs_f64() / ARCS as f64) * 1e9 );
-
-
-    // Check that they read the same
-    let start = std::time::Instant::now();
-    for node_id in 0..(NODES as u64) {
-        let _ = random_reader.get_successors_iter(node_id).unwrap().collect::<Vec<_>>();
-    }
-    println!("Random:    {:>20} ns/arcs", (start.elapsed().as_secs_f64() / ARCS as f64) * 1e9);
+    Ok(())
 }
