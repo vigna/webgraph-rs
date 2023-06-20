25539eebf8bf5ec9ca1d237eefb8c961cf910ec4
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-06-20 13:55:11 +0200

    cleaned up the bench webgraph file

--- src/bin/bench_webgraph.rs
+++ src/bin/bench_webgraph.rs
@@ -1,17 +1,9 @@
 use clap::Parser;
-use dsi_bitstream::prelude::*;
-use mmap_rs::*;
 use rand::rngs::SmallRng;
 use rand::Rng;
 use rand::SeedableRng;
-use std::fs::File;
 use std::hint::black_box;
-use std::io::BufReader;
-use std::io::Seek;
 use webgraph::prelude::*;
-
-type ReadType = u32;
-type BufferType = u64;
 
 #[derive(Parser, Debug)]
 #[command(about = "Benchmarks the Rust Webgraph implementation", long_about = None)]
@@ -44,18 +36,6 @@ struct Args {
     check: bool,
 }
 
-fn mmap_file(path: &str) -> Mmap {
-    let mut file = std::fs::File::open(path).unwrap();
-    let file_len = file.seek(std::io::SeekFrom::End(0)).unwrap();
-    unsafe {
-        MmapOptions::new(file_len as _)
-            .unwrap()
-            .with_file(file, 0)
-            .map()
-            .unwrap()
-    }
-}
-
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
@@ -65,58 +45,19 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         .init()
         .unwrap();
 
-    let f = File::open(format!("{}.properties", args.basename))?;
-    let map = java_properties::read(BufReader::new(f))?;
-    let comp_flags = CompFlags::from_properties(&map)?;
-    let num_nodes = map.get("nodes").unwrap().parse::<usize>()?;
-    let num_arcs = map.get("arcs").unwrap().parse::<usize>()?;
-    let min_interval_length = map.get("minintervallength").unwrap().parse::<usize>()?;
-    let compression_window = map.get("windowsize").unwrap().parse::<usize>()?;
-
-    // Read the offsets
-    let data_graph = mmap_file(&format!("{}.graph", args.basename));
-
-    let graph_slice = unsafe {
-        core::slice::from_raw_parts(
-            data_graph.as_ptr() as *const ReadType,
-            (data_graph.len() + core::mem::size_of::<ReadType>() - 1)
-                / core::mem::size_of::<ReadType>(),
-        )
-    };
-
     if args.check {
         // Create a sequential reader
-        let mut seq_reader = WebgraphSequentialIter::new(
-            DynamicCodesReader::new(
-                BufferedBitStreamRead::<BE, BufferType, _>::new(MemWordReadInfinite::new(
-                    &graph_slice,
-                )),
-                &comp_flags,
-            )?,
-            compression_window,
-            min_interval_length,
-            num_nodes,
-        );
-
+        let seq_graph = webgraph::bvgraph::load_seq(&args.basename)?;
+        let seq_graph =
+            seq_graph.map_codes_reader_builder(|cbr| DynamicCodesReaderSkipperBuilder::from(cbr));
         // create a random access reader;
         let random_reader = webgraph::bvgraph::load(&args.basename)?;
 
-        // Create a degrees reader
-        let mut deg_reader = WebgraphDegreesIter::new(
-            DynamicCodesReaderSkipper::new(
-                BufferedBitStreamRead::<BE, BufferType, _>::new(MemWordReadInfinite::new(
-                    &graph_slice,
-                )),
-                &comp_flags,
-            )?,
-            min_interval_length,
-            compression_window,
-            num_nodes,
-        );
-
         // Check that sequential and random-access interfaces return the same result
-        for node_id in 0..num_nodes {
-            let seq = seq_reader.next_successors()?;
+        let mut seq_iter = seq_graph.iter_nodes();
+        let mut deg_reader = seq_graph.iter_degrees();
+        for node_id in 0..seq_graph.num_nodes() {
+            let seq = seq_iter.next_successors()?;
             let random = random_reader.successors(node_id).collect::<Vec<_>>();
 
             assert_eq!(deg_reader.next_degree()?, seq.len(), "{}", node_id);
@@ -137,27 +78,19 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
                 (start.elapsed().as_secs_f64() / c as f64) * 1e9
             );
 
-            assert_eq!(c, num_arcs);
+            assert_eq!(c, seq_graph.num_arcs_hint().unwrap());
         }
     } else if args.degrees_only {
         // Sequential speed test
         for _ in 0..args.repeats {
-            // Create a degrees reader
-            let mut deg_reader = WebgraphDegreesIter::new(
-                DynamicCodesReaderSkipper::new(
-                    BufferedBitStreamRead::<BE, BufferType, _>::new(MemWordReadInfinite::new(
-                        &graph_slice,
-                    )),
-                    &comp_flags,
-                )?,
-                min_interval_length,
-                compression_window,
-                num_nodes,
-            );
+            let seq_graph = webgraph::bvgraph::load_seq(&args.basename)?;
+            let seq_graph = seq_graph
+                .map_codes_reader_builder(|cbr| DynamicCodesReaderSkipperBuilder::from(cbr));
+            let mut deg_reader = seq_graph.iter_degrees();
 
             let mut c: usize = 0;
             let start = std::time::Instant::now();
-            for _ in 0..num_nodes {
+            for _ in 0..seq_graph.num_nodes() {
                 c += deg_reader.next_degree()?;
             }
             println!(
@@ -165,7 +98,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
                 (start.elapsed().as_secs_f64() / c as f64) * 1e9
             );
 
-            assert_eq!(c, num_arcs);
+            assert_eq!(c, seq_graph.num_arcs_hint().unwrap());
         }
     } else {
         let graph = webgraph::bvgraph::load(&args.basename)?;
@@ -182,14 +115,16 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
             if args.first {
                 for _ in 0..args.n {
                     u += graph
-                        .successors(random.gen_range(0..num_nodes))
+                        .successors(random.gen_range(0..graph.num_nodes()))
                         .next()
                         .unwrap_or(0);
                     c += 1;
                 }
             } else {
                 for _ in 0..args.n {
-                    c += graph.successors(random.gen_range(0..num_nodes)).count();
+                    c += graph
+                        .successors(random.gen_range(0..graph.num_nodes()))
+                        .count();
                 }
             }
 
