5c1355a971ab2b72072688c973ba3937fd1d1251
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-06-21 15:11:44 +0200

    added new test of consistency of offsets and ef

---
+++ tests/test_offsets.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use dsi_bitstream::prelude::*;
+use std::io::prelude::*;
+use sux::prelude::*;
+use webgraph::prelude::*;
+
+#[test]
+fn test_offsets() -> Result<()> {
+    // load the graph
+    let graph = webgraph::bvgraph::load("tests/data/cnr-2000")?;
+
+    // Read the offsets gammas
+    let mut offsets_file = std::fs::File::open("tests/data/cnr-2000.offsets")?;
+    let mut offsets_data = vec![0; offsets_file.metadata()?.len() as usize];
+    offsets_file.read(&mut offsets_data)?;
+
+    let mut offsets = Vec::with_capacity(graph.num_nodes());
+    let mut reader =
+        BufferedBitStreamRead::<BE, u64, _>::new(MemWordReadInfinite::new(&offsets_data));
+    let mut offset = 0;
+    for _ in 0..graph.num_nodes() + 1 {
+        offset += reader.read_gamma().unwrap() as usize;
+        offsets.push(offset as u64);
+    }
+    println!("{:?}", offsets.len());
+
+    // Load Elias-fano
+    let ef_offsets = sux::prelude::map::<_, webgraph::EF<&[u64]>>(
+        "tests/data/cnr-2000.ef",
+        &sux::prelude::Flags::TRANSPARENT_HUGE_PAGES,
+    )?;
+
+    for i in 0..graph.num_nodes() {
+        assert_eq!(offsets[i], ef_offsets.select(i).unwrap() as _);
+    }
+
+    // Check that they read the same
+    for (node_id, seq_succ) in graph.iter_nodes() {
+        let rand_succ = graph.successors(node_id).collect::<Vec<_>>();
+        assert_eq!(rand_succ, seq_succ.collect::<Vec<_>>());
+    }
+
+    Ok(())
+}
