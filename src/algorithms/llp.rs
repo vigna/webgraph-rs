e13120796d7df917fe16ce8b635743af4d81a51e
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-09-07 17:00:13 +0200

    LLP

--- src/algorithms/llp.rs
+++ src/algorithms/llp.rs
@@ -1,6 +1,8 @@
+use crate::prelude::PermutedGraph;
 use crate::traits::*;
-use anyhow::{bail, Result};
+use anyhow::Result;
 use dsi_progress_logger::ProgressLogger;
+use epserde::*;
 use log::info;
 use rand::rngs::SmallRng;
 use rand::seq::SliceRandom;
@@ -10,7 +12,6 @@ use rayon::slice::ParallelSliceMut;
 use std::collections::HashMap;
 use std::sync::atomic::Ordering;
 use std::sync::atomic::{AtomicBool, AtomicU64, AtomicUsize};
-use std::sync::Mutex;
 
 /// Write the permutation computed by the LLP algorithm inside `perm`,
 /// and return the labels of said permutation.
@@ -21,8 +22,7 @@ use std::sync::Mutex;
 #[allow(clippy::too_many_arguments)]
 pub fn layered_label_propagation<G>(
     graph: &G,
-    perm: &mut [usize],
-    gamma: f64,
+    gammas: Vec<f64>,
     num_cpus: Option<usize>,
     max_iters: usize,
     chunk_size: usize,
@@ -32,164 +32,218 @@ pub fn layered_label_propagation<G>(
 where
     G: RandomAccessGraph + Sync,
 {
-    let num_cpus = num_cpus.unwrap_or_else(num_cpus::get);
     let num_nodes = graph.num_nodes();
 
-    if perm.len() != num_nodes {
-        bail!(
-            "The permutation slice is long {} but we expect it to be {}.",
-            perm.len(),
-            num_nodes
-        );
-    }
     // init the permutation with the indices
-    perm.iter_mut().enumerate().for_each(|(i, x)| *x = i);
+    let mut update_perm = (0..num_nodes).collect::<Vec<_>>();
 
     let mut can_change = Vec::with_capacity(num_nodes as _);
     can_change.extend((0..num_nodes).map(|_| AtomicBool::new(true)));
     let label_store = LabelStore::new(num_nodes as _);
 
     // build a thread_pool so we avoid having to re-create the threads
+    let num_cpus = num_cpus.unwrap_or_else(num_cpus::get);
     let thread_pool = rayon::ThreadPoolBuilder::new()
         .num_threads(num_cpus)
         .build()?;
 
     // init the progress logger
-    let mut glob_pr = ProgressLogger::default().display_memory();
-    glob_pr.item_name = "update";
-    glob_pr.start("Starting updates...");
+    let mut iter_pr = ProgressLogger::default().display_memory();
+    iter_pr.item_name = "update";
+    iter_pr.start("Starting updates...");
+
+    let mut graph_pr = ProgressLogger::default();
+    graph_pr.item_name = "node";
+    graph_pr.local_speed = true;
+    graph_pr.expected_updates = Some(num_nodes);
 
     let seed = AtomicU64::new(seed);
-    for _ in 0..max_iters {
-        thread_pool.install(|| {
-            // parallel shuffle using the num_cpus
-            perm.par_chunks_mut(chunk_size).for_each(|chunk| {
-                let seed = seed.fetch_add(1, Ordering::Relaxed);
-                let mut rand = SmallRng::seed_from_u64(seed);
-                chunk.shuffle(&mut rand);
+    let mut costs = Vec::with_capacity(gammas.len());
+    for gamma in &gammas {
+        let mut prev_obj_func = 0.0;
+        for _ in 0..max_iters {
+            thread_pool.install(|| {
+                // parallel shuffle using the num_cpus
+                update_perm.par_chunks_mut(chunk_size).for_each(|chunk| {
+                    let seed = seed.fetch_add(1, Ordering::Relaxed);
+                    let mut rand = SmallRng::seed_from_u64(seed);
+                    chunk.shuffle(&mut rand);
+                });
             });
-        });
-        let mut pr = ProgressLogger::default();
-        pr.item_name = "node";
-        pr.local_speed = true;
-        pr.expected_updates = Some(num_nodes);
-        pr.start("Updating...");
-        let prlock = Mutex::new(&mut pr);
 
-        // If this iteration modified anything (early stop)
-        let modified = AtomicUsize::new(0);
-        let delta = Mutex::new(0.0);
-        let pos = AtomicUsize::new(0);
+            graph_pr.start("Updating...");
 
-        // in parallel run the computation
-        thread_pool.scope(|scope| {
-            for _ in 0..num_cpus {
-                scope.spawn(|_s| {
-                    let mut local_delta = 0.0;
-                    let mut map = HashMap::new();
-                    let mut rand = SmallRng::seed_from_u64(seed.fetch_add(1, Ordering::Relaxed));
+            // If this iteration modified anything (early stop)
+            let modified = AtomicUsize::new(0);
 
-                    loop {
-                        let next_pos = pos.fetch_add(granularity, Ordering::Relaxed);
-                        if next_pos >= num_nodes {
-                            let mut delta = delta.lock().unwrap();
-                            *delta += local_delta;
-                            break;
+            let obj_func = crate::graph::par_graph_apply(
+                graph,
+                |range| {
+                    let mut map = HashMap::with_capacity(1024);
+                    let mut rand = SmallRng::seed_from_u64(range.start as u64);
+                    let mut local_obj_func = 0.0;
+                    for &node in &update_perm[range] {
+                        // if the node can't change we can skip it
+                        if !can_change[node].load(Ordering::Relaxed) {
+                            continue;
                         }
-                        let end_pos = (next_pos + granularity).min(perm.len());
+                        // set that the node can't change by default and we'll unset later it if it can
+                        can_change[node].store(false, Ordering::Relaxed);
 
-                        let chunk = &perm[next_pos..end_pos];
-
-                        for &node in chunk {
-                            // if the node can't change we can skip it
-                            if !can_change[node].load(Ordering::Relaxed) {
-                                continue;
-                            }
-                            // set that the node can't change by default and we'll unset later it if it can
-                            can_change[node].store(false, Ordering::Relaxed);
-
-                            let successors = graph.successors(node);
-                            if successors.len() == 0 {
-                                continue;
-                            }
-
-                            // get the label of this node
-                            let curr_label = label_store.label(node as _);
-                            // get the count of how many times a
-                            // label appears in the successors
-                            map.clear();
-                            for succ in successors {
-                                map.entry(label_store.label(succ))
-                                    .and_modify(|counter| *counter += 1)
-                                    .or_insert(1);
-                            }
-
-                            let mut max = f64::MIN;
-                            let mut old = 0.0;
-                            let mut majorities = vec![];
-                            // compute the most entropic label
-                            for (&label, &count) in map.iter() {
-                                let volume = label_store.volume(label);
-                                let val =
-                                    (1.0 + gamma) * count as f64 - gamma * (volume + 1) as f64;
-
-                                if max == val {
-                                    majorities.push(label);
-                                }
-
-                                if max < val {
-                                    majorities.clear();
-                                    max = val;
-                                    majorities.push(label);
-                                }
-
-                                if label == curr_label {
-                                    old = val;
-                                }
-                            }
-                            // randomly break ties
-                            let next_label = *majorities.choose(&mut rand).unwrap();
-                            // if the label changed we need to update the label store
-                            // and signal that this could change the neighbour nodes
-                            if next_label != curr_label {
-                                modified.fetch_add(1, Ordering::Relaxed);
-                                for succ in graph.successors(node) {
-                                    can_change[succ].store(true, Ordering::Relaxed);
-                                }
-
-                                label_store.set(node as _, next_label);
-                            }
-
-                            local_delta += max - old;
+                        let successors = graph.successors(node);
+                        if successors.len() == 0 {
+                            continue;
                         }
-                        // update the progress logger with how many nodes we processed
-                        prlock.lock().unwrap().update_with_count(perm.len());
+
+                        // get the label of this node
+                        let curr_label = label_store.label(node as _);
+                        // get the count of how many times a
+                        // label appears in the successors
+                        map.clear();
+                        for succ in successors {
+                            map.entry(label_store.label(succ))
+                                .and_modify(|counter| *counter += 1)
+                                .or_insert(1_usize);
+                        }
+                        // add the current label to the map
+                        map.entry(label_store.label(node)).or_insert(0_usize);
+
+                        let mut max = f64::NEG_INFINITY;
+                        let mut old = 0.0;
+                        let mut majorities = vec![];
+                        // compute the most entropic label
+                        for (&label, &count) in map.iter() {
+                            let volume = label_store.volume(label);
+                            let val = (1.0 + gamma) * count as f64 - gamma * (volume + 1) as f64;
+
+                            if max == val {
+                                majorities.push(label);
+                            }
+
+                            if val > max {
+                                majorities.clear();
+                                max = val;
+                                majorities.push(label);
+                            }
+
+                            if label == curr_label {
+                                old = val;
+                            }
+                        }
+                        // randomly break ties
+                        let next_label = *majorities.choose(&mut rand).unwrap();
+                        // if the label changed we need to update the label store
+                        // and signal that this could change the neighbour nodes
+                        if next_label != curr_label {
+                            modified.fetch_add(1, Ordering::Relaxed);
+                            for succ in graph.successors(node) {
+                                can_change[succ].store(true, Ordering::Relaxed);
+                            }
+                            label_store.set(node as _, next_label);
+                        }
+                        local_obj_func += max - old;
                     }
-                })
+                    local_obj_func
+                },
+                |delta0, delta1| delta0 + delta1,
+                &thread_pool,
+                granularity,
+                Some(&mut graph_pr),
+            );
+
+            let gain = 1.0 - (prev_obj_func / obj_func);
+            info!(
+                "Modified: {} Gain: {} PObjFunc: {} ObjFunc: {}",
+                modified.load(Ordering::Relaxed),
+                gain,
+                prev_obj_func,
+                obj_func,
+            );
+            prev_obj_func = obj_func;
+            graph_pr.done_with_count(num_nodes as _);
+            iter_pr.update_and_display();
+
+            if modified.load(Ordering::Relaxed) == 0 {
+                break;
             }
+            if gain < 0.001 {
+                break;
+            }
+        }
+
+        iter_pr.done();
+
+        // create sorted clusters by contiguous labels
+        update_perm.par_sort_unstable_by(|&a, &b| {
+            label_store.label(a as _).cmp(&label_store.label(b as _))
         });
 
-        pr.done_with_count(num_nodes as _);
-        info!(
-            "Modified: {} Delta: {}",
-            modified.load(Ordering::Relaxed),
-            delta.lock().unwrap()
-        );
-        glob_pr.update_and_display();
-        if modified.load(Ordering::Relaxed) == 0 {
-            break;
-        }
+        let labels =
+            unsafe { std::mem::transmute::<&[AtomicUsize], &[usize]>(&label_store.labels) };
+
+        let pgraph = PermutedGraph {
+            graph: graph,
+            perm: &update_perm,
+        };
+        let cost = compute_log_gap_cost(&thread_pool, &pgraph, None);
+        info!("Gamma: {} Log gap cost: {}", gamma, cost);
+        costs.push(cost);
+
+        // storing the perms
+        let mut file = std::fs::File::create(format!("labels_{}.bin", gamma))?;
+        labels.to_vec().serialize(&mut file)?; // TODO!: REMOVE to_vec
     }
 
-    glob_pr.done();
+    // compute the indices that sorts the gammas by cost
+    let mut indices = (0..costs.len()).collect::<Vec<_>>();
+    // sort in descending order
+    indices.sort_by(|a, b| costs[*b].total_cmp(&costs[*a]));
 
-    // create sorted clusters by contiguous labels
-    perm.par_sort_unstable_by(|&a, &b| label_store.label(a as _).cmp(&label_store.label(b as _)));
+    // the best gamma is the last because it has the min cost
+    let best_gamma = gammas[*indices.last().unwrap()];
+    info!("Best gamma: {}", best_gamma);
+    // reuse the update_perm to store the final permutation
+    let mut temp_perm = update_perm;
 
-    let labels =
-        unsafe { std::mem::transmute::<Box<[AtomicUsize]>, Box<[usize]>>(label_store.labels) };
+    let mut result_labels = load::<Vec<usize>>(format!("labels_{}.bin", best_gamma))?.to_vec();
+    for index in indices {
+        let labels = load::<Vec<usize>>(format!("labels_{}.bin", gammas[index]))?;
+        combine(&mut result_labels, *labels, &mut temp_perm)?;
+        let best_labels = load::<Vec<usize>>(format!("labels_{}.bin", best_gamma))?;
+        combine(&mut result_labels, *best_labels, &mut temp_perm)?;
+    }
 
-    Ok(labels)
+    // re-init the permutation
+    temp_perm.iter_mut().enumerate().for_each(|(i, x)| *x = i);
+    temp_perm.par_sort_unstable_by(|&a, &b| result_labels[a].cmp(&result_labels[b]));
+
+    Ok(temp_perm.into_boxed_slice())
+}
+
+/// combine the labels from two permutations into a single one
+fn combine(result: &mut [usize], labels: &[usize], temp_perm: &mut [usize]) -> Result<usize> {
+    // re-init the permutation
+    temp_perm.iter_mut().enumerate().for_each(|(i, x)| *x = i);
+    // permute by the devilish function
+    temp_perm.par_sort_by(|&a, &b| {
+        (result[labels[a]].cmp(&result[labels[b]]))
+            .then_with(|| labels[a].cmp(&labels[b]))
+            .then_with(|| result[a].cmp(&result[b]))
+    });
+    let mut prev_labels = (result[temp_perm[0]], labels[temp_perm[0]]);
+    let mut curr_label = 0;
+    temp_perm[0] = curr_label;
+
+    for i in 1..temp_perm.len() {
+        let curr_labels = (result[temp_perm[i]], labels[temp_perm[i]]);
+        if prev_labels != curr_labels {
+            curr_label += 1;
+            prev_labels = curr_labels
+        }
+        result[temp_perm[i]] = curr_label;
+    }
+
+    Ok(curr_label + 1)
 }
 
 struct LabelStore {
@@ -211,16 +265,19 @@ impl LabelStore {
         }
     }
 
+    #[inline(always)]
     fn set(&self, node: usize, new_label: usize) {
         let old_label = self.labels[node].swap(new_label, Ordering::Relaxed);
         self.volumes[old_label].fetch_sub(1, Ordering::Relaxed);
         self.volumes[new_label].fetch_add(1, Ordering::Relaxed);
     }
 
+    #[inline(always)]
     fn label(&self, node: usize) -> usize {
         self.labels[node].load(Ordering::Relaxed)
     }
 
+    #[inline(always)]
     fn volume(&self, label: usize) -> usize {
         self.volumes[label].load(Ordering::Relaxed)
     }
@@ -228,3 +285,39 @@ impl LabelStore {
 
 unsafe impl Send for LabelStore {}
 unsafe impl Sync for LabelStore {}
+
+fn compute_log_gap_cost<G: SequentialGraph + Sync>(
+    thread_pool: &rayon::ThreadPool,
+    graph: &G,
+    pr: Option<&mut ProgressLogger>,
+) -> f64 {
+    let cost = crate::graph::par_graph_apply(
+        graph,
+        |range| {
+            let res = graph
+                .iter_nodes_from(range.start)
+                .take(range.len())
+                .map(|(x, succ)| {
+                    let mut cost = 0;
+                    let mut sorted: Vec<_> = succ.collect();
+                    if !sorted.is_empty() {
+                        sorted.sort();
+                        cost +=
+                            ((x as isize - sorted[0] as isize).unsigned_abs() + 1).ilog2() as usize;
+                        cost += sorted
+                            .windows(2)
+                            .map(|w| (w[1] - w[0]).ilog2() as usize)
+                            .sum::<usize>();
+                    }
+                    cost
+                })
+                .sum::<usize>() as f64;
+            res
+        },
+        |a, b| a + b,
+        thread_pool,
+        1_000,
+        pr,
+    );
+    cost
+}
--- src/bin/llp.rs
+++ src/bin/llp.rs
@@ -35,14 +35,6 @@ struct Args {
     seed: u64,
 }
 
-fn ceil_log2(x: usize) -> usize {
-    if x <= 2 {
-        x - 1
-    } else {
-        64 - (x - 1).leading_zeros() as usize
-    }
-}
-
 pub fn main() -> Result<()> {
     let start = std::time::Instant::now();
     let args = Args::parse();
@@ -56,12 +48,10 @@ pub fn main() -> Result<()> {
     // load the graph
     let graph = webgraph::graph::bvgraph::load(&args.basename)?;
 
-    let mut perm = vec![0; graph.num_nodes()];
     // compute the LLP
-    let labels = layered_label_propagation(
+    let llp_perm = layered_label_propagation(
         &graph,
-        &mut perm,
-        args.gamma,
+        vec![0.0, 0.0, 0.0], // TODO!: remove
         args.num_cpus,
         args.max_iters,
         args.chunk_size,
@@ -71,54 +61,10 @@ pub fn main() -> Result<()> {
 
     log::info!("Elapsed: {}", start.elapsed().as_secs_f64());
     // dump the labels
-    let labels = unsafe { std::mem::transmute::<Box<[usize]>, Box<[u8]>>(labels) };
-    std::fs::File::create(format!("{}-{}.labels", args.basename, 0))?.write_all(&labels)?;
-
-    let pgraph = PermutedGraph {
-        graph: &graph,
-        perm: &perm,
-    };
-    let pgraph_ref = &pgraph;
-    // dump the permutation
-    let num_cpus = args
-        .num_cpus
-        .unwrap_or_else(|| std::thread::available_parallelism().unwrap().get())
-        .min(graph.num_nodes());
-    let nodes_per_thread = graph.num_nodes() / num_cpus;
-    let cost = std::thread::scope(|s| {
-        let mut handles = Vec::with_capacity(num_cpus);
-
-        for i in 0..num_cpus {
-            let start_node = i * nodes_per_thread;
-            let handle = s.spawn(move || {
-                pgraph_ref
-                    .iter_nodes_from(start_node)
-                    .take(nodes_per_thread)
-                    .map(|(x, succ)| {
-                        let mut cost = 0;
-                        let mut sorted: Vec<_> = succ.collect();
-                        if !sorted.is_empty() {
-                            sorted.sort();
-                            cost += ceil_log2((x as isize - sorted[0] as isize).unsigned_abs());
-                            cost += sorted
-                                .windows(2)
-                                .map(|w| ceil_log2(w[1] - w[0]))
-                                .sum::<usize>();
-                        }
-                        cost
-                    })
-                    .sum::<usize>()
-            });
-            handles.push(handle);
-        }
-
-        let mut res = 0;
-        for handle in handles {
-            res += handle.join().unwrap();
-        }
-        res
-    });
-    log::info!("The final cost is: {}", cost);
-
+    // TODO!: This can be done better maybe
+    let mut file = std::fs::File::create(format!("{}.llp.order", args.basename))?;
+    for word in llp_perm.into_iter() {
+        file.write(&word.to_be_bytes())?;
+    }
     Ok(())
 }
