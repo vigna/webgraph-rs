fca2f3e59d7a41128e7d26d259eb2f5033f7d0da
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-09-30 11:19:21 +0200

    Now we use load

--- tests/test_read_webgraph.rs
+++ tests/test_read_webgraph.rs
@@ -5,79 +5,12 @@
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
  */
 
-use std::collections::HashMap;
-
 use anyhow::Result;
-use dsi_bitstream::prelude::*;
-use epserde::prelude::MemCase;
-use sux::prelude::*;
 use webgraph::prelude::*;
-
-type ReadType = u32;
-type BufferType = u64;
-
-const NODES: usize = 325557;
-const ARCS: usize = 3216152;
-
-fn get_bvgraph() -> Result<impl RandomAccessGraph> {
-    // Read the offsets
-    let mut data = std::fs::read("tests/data/cnr-2000.offsets").unwrap();
-    // pad with zeros so we can read with ReadType words
-    while data.len() % core::mem::size_of::<ReadType>() != 0 {
-        data.push(0);
-    }
-    // we must do this becasue Vec<u8> is not guaranteed to be properly aligned
-    let data = data
-        .chunks(core::mem::size_of::<ReadType>())
-        .map(|chunk| ReadType::from_ne_bytes(chunk.try_into().unwrap()))
-        .collect::<Vec<_>>();
-
-    // Read the offsets gammas
-    let mut offsets = Vec::with_capacity(NODES);
-    let mut reader = BufBitReader::<BE, BufferType, _>::new(MemWordReaderInf::new(&data));
-    let mut offset = 0;
-    for _ in 0..NODES {
-        offset += reader.read_gamma().unwrap() as usize;
-        offsets.push(offset);
-    }
-
-    let mut builder = EliasFanoBuilder::new(offsets.len(), offset + 1);
-    for o in offsets {
-        builder.push(o)?;
-    }
-
-    let mut data = std::fs::read("tests/data/cnr-2000.graph").unwrap();
-    // pad with zeros so we can read with ReadType words
-    while data.len() % core::mem::size_of::<ReadType>() != 0 {
-        data.push(0);
-    }
-    // we must do this becasue Vec<u8> is not guaranteed to be properly aligned
-    let data = data
-        .chunks(core::mem::size_of::<ReadType>())
-        .map(|chunk| ReadType::from_ne_bytes(chunk.try_into().unwrap()))
-        .collect::<Vec<_>>();
-
-    let cf = CompFlags::from_properties(&HashMap::new()).unwrap();
-    let compression_window = cf.compression_window;
-    let min_interval_length = cf.min_interval_length;
-
-    let ef = builder.build();
-    let ef: webgraph::EF<Vec<usize>> = ef.convert_to().unwrap();
-
-    // create a random access reader
-    Ok(BVGraph::new(
-        <DynamicCodesReaderBuilder<BE, _>>::new(data, cf).unwrap(),
-        MemCase::encase(ef),
-        min_interval_length,
-        compression_window,
-        NODES,
-        ARCS,
-    ))
-}
 
 #[test]
 fn test_iter_nodes() -> Result<()> {
-    let bvgraph = get_bvgraph()?;
+    let bvgraph = webgraph::graph::bvgraph::load("tests/data/cnr-2000")?;
 
     let mut seen_node_ids = Vec::new();
 
@@ -99,7 +32,7 @@ fn test_iter_nodes() -> Result<()> {
 
 #[test]
 fn test_iter_nodes_from() -> Result<()> {
-    let bvgraph = get_bvgraph()?;
+    let bvgraph = webgraph::graph::bvgraph::load("tests/data/cnr-2000")?;
     for i in [0, 1, 2, 5, 10, 100] {
         let mut seen_node_ids = Vec::new();
         // Check that they read the same
