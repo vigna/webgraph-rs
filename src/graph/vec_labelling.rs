20bb440a289eeecbeb9abc1575458d5a14449197
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-11-08 15:03:28 +0100

    Starting labels

---
+++ src/graph/vec_labelling.rs
@@ -0,0 +1,331 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Inria
+ * SPDX-FileCopyrightText: 2023 Sebastiano Vigna
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+/*
+use crate::{for_iter, prelude::*};
+use alloc::collections::BTreeSet;
+use lender::*;
+
+/// A vector-based mutable [`Graph`]/[`LabeledGraph`] implementation.
+///
+/// Successors are represented using a [`BTreeSet`]. Choosing `()`
+/// as the label type will result in a [`Graph`] implementation.
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct VecGraph<L: Clone> {
+    /// The number of arcs in the graph.
+    number_of_arcs: usize,
+    /// For each node, its list of successors.
+    succ: Vec<BTreeSet<DstWithLabel<L>>>,
+}
+
+impl<L: Clone> core::default::Default for VecGraph<L> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<L: Clone> VecGraph<L> {
+    /// Create a new empty graph.
+    pub fn new() -> Self {
+        Self {
+            number_of_arcs: 0,
+            succ: vec![],
+        }
+    }
+
+    /// Create a new empty graph with `n` nodes.
+    pub fn empty(n: usize) -> Self {
+        Self {
+            number_of_arcs: 0,
+            succ: Vec::from_iter((0..n).map(|_| BTreeSet::new())),
+        }
+    }
+
+    /// Convert a COO arc list into a graph by sorting and deduplicating.
+    pub fn from_arc_and_label_list(arcs: &[(usize, usize, L)]) -> Self {
+        let mut g = Self::new();
+        g.add_arc_and_label_list(arcs);
+        g
+    }
+
+    /// Add an arc to the graph and return a reference to self to allow a
+    /// builder-like usage.
+    pub fn add_arc_and_label_list(&mut self, arcs: &[(usize, usize, L)]) -> &mut Self {
+        for (u, v, l) in arcs {
+            self.add_arc_with_label(*u, *v, l.clone());
+        }
+        self
+    }
+
+    /// Add an arc to the graph and return if it was a new one or not.
+    /// `true` => already exist, `false` => new arc.
+    pub fn add_arc_with_label(&mut self, u: usize, v: usize, l: L) -> bool {
+        self.add_node(u.max(v));
+        let result = self.succ[u].insert(DstWithLabel(v, l));
+        self.number_of_arcs += result as usize;
+        result
+    }
+
+    /// Remove an arc from the graph and return if it was present or not.
+    /// Return Nones if the either nodes (`u` or `v`) do not exist.
+    pub fn remove_labeled_arc(&mut self, u: usize, v: usize, l: L) -> Option<bool> {
+        if u >= self.succ.len() || v >= self.succ.len() {
+            return None;
+        }
+        let result = self.succ[u].remove(&DstWithLabel(v, l));
+        self.number_of_arcs -= result as usize;
+        Some(result)
+    }
+
+    /// Add a node to the graph without successors and return if it was a new
+    /// one or not.
+    pub fn add_node(&mut self, node: usize) -> bool {
+        let len = self.succ.len();
+        self.succ.extend((len..=node).map(|_| BTreeSet::new()));
+        len <= node
+    }
+    /*
+    /// Convert the `iter_nodes` iterator of a graph into a [`VecGraph`].
+    pub fn from_labeled_node_iter<I>(iter_nodes: I) -> Self
+    where
+        I: IntoLender,
+        for<'next> Lend<'next, I::Lender>: Tuple2<_0 = usize>,
+        for<'next> <Lend<'next, I::Lender> as Tuple2>::_1: IntoIterator<Item = (usize, L)>,
+        for<'next> <Lend<'next, I::Lender> as Tuple2>::_1: Labeled<Label = L>,
+    {
+        let mut g = Self::new();
+        g.add_labeled_node_iter(iter_nodes);
+        g
+    }
+
+    /// Add the nodes and sucessors from the `iter_nodes` iterator of a graph
+    pub fn add_labeled_node_iter<I>(&mut self, iter_nodes: I) -> &mut Self
+    where
+        I: IntoLender,
+        for<'next> Lend<'next, I::Lender>: Tuple2<_0 = usize>,
+        for<'next> <Lend<'next, I::Lender> as Tuple2>::_1:
+            IntoIterator<Item = usize> + LabeledSuccessors,
+        for<'next> <Lend<'next, I::Lender> as Tuple2>::_1: LabeledSuccessors<Label = L>,
+    {
+        for_iter! { (node, succ) in iter_nodes =>
+            self.add_node(node);
+            for (v, l) in succ.labeled() {
+                self.add_arc_with_label(node, v, l);
+            }
+        }
+        self
+    }
+
+    /// Add the nodes, arcs, and labels in a graph to a [`VecGraph`].
+    pub fn add_labeled_graph<S: LabeledSequentialGraph<Label = L>>(
+        &mut self,
+        graph: &S,
+    ) -> &mut Self
+    where
+        for<'next> <S as SequentialGraph>::Successors<'next>: LabeledSuccessors<Label = L>,
+    {
+        self.add_labeled_node_iter::<<S as SequentialGraph>::Iterator<'_>>(graph.iter())
+    }*/
+}
+
+impl VecGraph<()> {
+    /// Convert a COO arc list into a graph by sorting and deduplicating.
+    pub fn from_arc_list(arcs: &[(usize, usize)]) -> Self {
+        let mut g = Self::new();
+        g.add_arc_list(arcs);
+        g
+    }
+
+    /// Add an arc to the graph and return a reference to self to allow a
+    /// builder-like usage.
+    pub fn add_arc_list(&mut self, arcs: &[(usize, usize)]) -> &mut Self {
+        for (u, v) in arcs {
+            self.add_arc(*u, *v);
+        }
+        self
+    }
+
+    /// Convert an iterator on nodes and successors in a [`VecGraph`].
+    pub fn from_node_iter<L>(iter_nodes: L) -> Self
+    where
+        L: IntoLender,
+        for<'next> Lend<'next, L::Lender>: Tuple2<_0 = usize>,
+        for<'next> <Lend<'next, L::Lender> as Tuple2>::_1: IntoIterator<Item = usize>,
+    {
+        let mut g = Self::new();
+        g.add_node_iter(iter_nodes);
+        g
+    }
+
+    /// Add the nodes and successors from an iterator to a [`VecGraph`].
+    pub fn add_node_iter<L>(&mut self, iter_nodes: L) -> &mut Self
+    where
+        L: IntoLender,
+        for<'next> Lend<'next, L::Lender>: Tuple2<_0 = usize>,
+        for<'next> <Lend<'next, L::Lender> as Tuple2>::_1: IntoIterator<Item = usize>,
+    {
+        for_iter! { (node, succ) in iter_nodes =>
+            self.add_node(node);
+            for v in succ {
+                self.add_arc(node, v);
+            }
+        }
+        self
+    }
+
+    /// Add an arc to the graph and return if it was a new one or not.
+    /// `true` => already exist, `false` => new arc.
+    pub fn add_arc(&mut self, u: usize, v: usize) -> bool {
+        let max = u.max(v);
+        if max >= self.succ.len() {
+            self.succ.resize(max + 1, BTreeSet::new());
+        }
+        let result = self.succ[u].insert(DstWithLabel(v, ()));
+        self.number_of_arcs += result as usize;
+        result
+    }
+
+    /// Remove an arc from the graph and return if it was present or not.
+    /// Return Nones if the either nodes (`u` or `v`) do not exist.
+    pub fn remove_arc(&mut self, u: usize, v: usize) -> Option<bool> {
+        if u >= self.succ.len() || v >= self.succ.len() {
+            return None;
+        }
+        let result = self.succ[u].remove(&DstWithLabel(v, ()));
+        self.number_of_arcs -= result as usize;
+        Some(result)
+    }
+}
+
+/*impl<'lend, 'a> Lending<'lend> for &'a VecGraph<()> {
+    type Lend = (usize, Successors<'lend, ()>);
+}*/
+
+impl<'a> IntoLender for &'a VecGraph<()> {
+    type Lender = <VecGraph<()> as SequentialLabelling>::Iterator<'a>;
+
+    #[inline(always)]
+    fn into_lender(self) -> Self::Lender {
+        self.iter()
+    }
+}
+/*
+impl<L: Clone> Labeled for VecGraph<L> {
+    type Label = L;
+}
+*/
+
+impl<L: Clone + 'static> RandomAccessGraph for VecGraph<L> {
+    type Successors<'a> = Successors<'a, L> where Self: 'a;
+
+    #[inline(always)]
+    fn num_arcs(&self) -> usize {
+        self.number_of_arcs
+    }
+
+    #[inline(always)]
+    fn outdegree(&self, node: usize) -> usize {
+        self.succ[node].len()
+    }
+
+    #[inline(always)]
+    fn successors(&self, node: usize) -> <Self as RandomAccessGraph>::Successors<'_> {
+        Successors {
+            iter: self.succ[node].iter(),
+            label: unsafe {
+                #[allow(clippy::uninit_assumed_init)]
+                core::mem::MaybeUninit::uninit().assume_init()
+            },
+        }
+    }
+}
+
+impl<L: Clone + 'static> SequentialGraph for VecGraph<L> {
+    type Value = usize;
+    type Successors<'a> = Successors<'a, L>;
+    type Iterator<'a> = IteratorImpl<'a, Self>
+    where L: 'a;
+
+    #[inline(always)]
+    fn num_nodes(&self) -> usize {
+        self.succ.len()
+    }
+
+    #[inline(always)]
+    fn num_arcs_hint(&self) -> Option<usize> {
+        Some(self.num_arcs())
+    }
+
+    #[inline(always)]
+    fn iter_from(&self, from: usize) -> Self::Iterator<'_> {
+        IteratorImpl {
+            graph: self,
+            nodes: (from..self.num_nodes()),
+        }
+    }
+}
+
+pub struct Successors<'a, L: Clone> {
+    label: L,
+    iter: std::collections::btree_set::Iter<'a, DstWithLabel<L>>,
+}
+
+impl<'a, T: Clone> Iterator for Successors<'a, T> {
+    type Item = usize;
+    #[inline(always)]
+    fn next(&mut self) -> Option<Self::Item> {
+        let x = self.iter.next()?;
+        self.label = x.1.clone();
+        Some(x.0)
+    }
+}
+
+impl<'a, L: Clone> Labeled for Successors<'a, L> {
+    type Label = L;
+}
+
+impl<'a, T: Clone> LabeledSuccessors for Successors<'a, T> {
+    fn label(&self) -> Self::Label {
+        self.label.clone()
+    }
+}
+
+unsafe impl<'a, T: Clone> SortedSuccessors for Successors<'a, T> {}
+
+impl<'a, T: Clone> ExactSizeIterator for Successors<'a, T> {
+    #[inline(always)]
+    fn len(&self) -> usize {
+        self.iter.len()
+    }
+}
+
+#[derive(Clone, Debug)]
+struct DstWithLabel<L>(usize, L);
+
+impl<L> PartialEq for DstWithLabel<L> {
+    #[inline(always)]
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<L> Eq for DstWithLabel<L> {}
+
+impl<L> PartialOrd for DstWithLabel<L> {
+    #[inline(always)]
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<L> Ord for DstWithLabel<L> {
+    #[inline(always)]
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+*/
---
+++ src/traits/labelling.rs
@@ -0,0 +1,177 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Inria
+ * SPDX-FileCopyrightText: 2023 Sebastiano Vigna
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later
+ */
+
+/*!
+
+Basic traits to access graphs, both sequentially and
+in random-access fashion.
+
+*/
+
+use core::{
+    ops::Range,
+    sync::atomic::{AtomicUsize, Ordering},
+};
+use dsi_progress_logger::*;
+use lender::*;
+use std::sync::Mutex;
+
+/// A labelling that can be accessed sequentially.
+///
+/// Note that there is no guarantee that the iterator will return nodes in
+/// ascending order, or the successors of a node will be returned in ascending order.
+/// The marker traits [SortedIterator] and [SortedSuccessors] can be used to
+/// force these properties.
+///
+/// The iterator returned by [iter](SequentialGraph::iter) is [lending](Lender):
+/// to access the next pair, you must have finished to use the previous one. You
+/// can invoke [`Lender::into_iter`] to get a standard iterator, in general
+/// at the cost of some allocation and copying.
+pub trait SequentialLabelling {
+    type Value;
+    type Successors<'succ>: IntoIterator<Item = Self::Value>;
+    /// The type of the iterator over the successors of a node
+    /// returned by [the iterator on the graph](SequentialGraph::Iterator).
+    type Iterator<'node>: Lender
+        + for<'succ> Lending<'succ, Lend = (usize, Self::Successors<'succ>)>
+    where
+        Self: 'node;
+
+    /// Return the number of nodes in the graph.
+    fn num_nodes(&self) -> usize;
+
+    /// Return the number of arcs in the graph, if available.
+    fn num_arcs_hint(&self) -> Option<usize> {
+        None
+    }
+
+    /// Return an iterator over the graph.
+    ///
+    /// Iterators over the graph return pairs given by a node of the graph
+    /// and an [IntoIterator] over its successors.
+    fn iter(&self) -> Self::Iterator<'_> {
+        self.iter_from(0)
+    }
+
+    /// Return an iterator over the nodes of the graph starting at `from`
+    /// (included).
+    ///
+    /// Note that if the graph iterator [is not sorted](SortedIterator),
+    /// `from` is not the node id of the first node returned by the iterator,
+    /// but just the starting point of the iteration.
+    fn iter_from(&self, from: usize) -> Self::Iterator<'_>;
+
+    /// Given a graph, apply `func` to each chunk of nodes of size `granularity`
+    /// in parallel, and reduce the results using `reduce`.
+    fn par_graph_apply<F, R, T>(
+        &self,
+        func: F,
+        reduce: R,
+        thread_pool: &rayon::ThreadPool,
+        granularity: usize,
+        pl: Option<&mut ProgressLogger>,
+    ) -> T
+    where
+        F: Fn(Range<usize>) -> T + Send + Sync,
+        R: Fn(T, T) -> T + Send + Sync,
+        T: Send + Default,
+    {
+        let pl_lock = pl.map(Mutex::new);
+        let num_nodes = self.num_nodes();
+        let num_cpus = thread_pool
+            .current_num_threads()
+            .min(num_nodes / granularity)
+            .max(1);
+        let next_node = AtomicUsize::new(0);
+
+        thread_pool.scope(|scope| {
+            let mut res = Vec::with_capacity(num_cpus);
+            for _ in 0..num_cpus {
+                // create a channel to receive the result
+                let (tx, rx) = std::sync::mpsc::channel();
+                res.push(rx);
+
+                // create some references so that we can share them across threads
+                let pl_lock_ref = &pl_lock;
+                let next_node_ref = &next_node;
+                let func_ref = &func;
+                let reduce_ref = &reduce;
+
+                scope.spawn(move |_| {
+                    let mut result = T::default();
+                    loop {
+                        // compute the next chunk of nodes to process
+                        let start_pos = next_node_ref.fetch_add(granularity, Ordering::Relaxed);
+                        let end_pos = (start_pos + granularity).min(num_nodes);
+                        // exit if done
+                        if start_pos >= num_nodes {
+                            break;
+                        }
+                        // apply the function and reduce the result
+                        result = reduce_ref(result, func_ref(start_pos..end_pos));
+                        // update the progress logger if specified
+                        if let Some(pl_lock) = pl_lock_ref {
+                            pl_lock
+                                .lock()
+                                .unwrap()
+                                .update_with_count((start_pos..end_pos).len());
+                        }
+                    }
+                    // comunicate back that the thread finished
+                    tx.send(result).unwrap();
+                });
+            }
+            // reduce the results
+            let mut result = T::default();
+            for rx in res {
+                result = reduce(result, rx.recv().unwrap());
+            }
+            result
+        })
+    }
+}
+
+/// A [sequential graph](SequentialGraph) providing, additionally, random access to successor lists.
+pub trait RandomAccessLabelling: SequentialLabelling {
+    /// The type of the iterator over the successors of a node
+    /// returned by [successors](RandomAccessGraph::successors).
+    type Successors<'succ>: IntoIterator<Item = <Self as SequentialLabelling>::Value>
+    where
+        Self: 'succ;
+
+    /// Return the number of arcs in the graph.
+    fn num_arcs(&self) -> usize;
+
+    /// Return an [`IntoIterator`] over the successors of a node.
+    fn successors(&self, node_id: usize) -> <Self as RandomAccessLabelling>::Successors<'_>;
+
+    /// Return the number of successors of a node.
+    fn outdegree(&self, _node_id: usize) -> usize {
+        todo!();
+        // self.successors(node_id).count()
+    }
+}
+
+/// A struct used to make it easy to implement [a graph iterator](Lender)
+/// for a type that implements [`RandomAccessGraph`].
+pub struct IteratorImpl<'node, G: RandomAccessLabelling> {
+    pub labelling: &'node G,
+    pub nodes: core::ops::Range<usize>,
+}
+
+impl<'node, 'succ, G: RandomAccessLabelling> Lending<'succ> for IteratorImpl<'node, G> {
+    type Lend = (usize, <G as RandomAccessLabelling>::Successors<'succ>);
+}
+
+impl<'node, G: RandomAccessLabelling> Lender for IteratorImpl<'node, G> {
+    #[inline(always)]
+    fn next(&mut self) -> Option<Lend<'_, Self>> {
+        self.nodes
+            .next()
+            .map(|node_id| (node_id, self.labelling.successors(node_id)))
+    }
+}
