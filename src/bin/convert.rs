3f989d6ddf58c7d17301d6cb3f63702dd403bfb9
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2024-01-27 18:14:40 +0100

    Fixed output

--- src/bin/convert.rs
+++ src/bin/convert.rs
@@ -7,10 +7,11 @@
 use anyhow::Result;
 use clap::Parser;
 use dsi_bitstream::prelude::*;
-use webgraph::prelude::*;
+use dsi_progress_logger::*;
+use log::info;
 use std::fs::File;
 use std::io::BufWriter;
-use dsi_progress_logger::*;
+use webgraph::prelude::*;
 
 /// A decoder that encodes the read values using the given encoder.
 pub struct Converter<D: Decoder, E: Encoder> {
@@ -19,6 +20,7 @@ pub struct Converter<D: Decoder, E: Encoder> {
 }
 
 impl<D: Decoder, E: Encoder> Decoder for Converter<D, E> {
+    // TODO: implement correctly start_node/end_node
     #[inline(always)]
     fn read_outdegree(&mut self) -> u64 {
         let res = self.decoder.read_outdegree();
@@ -74,7 +76,7 @@ impl<D: Decoder, E: Encoder> Decoder for Converter<D, E> {
         res
     }
 }
- 
+
 #[derive(Parser, Debug)]
 #[command(about = "Invert the endianness of a BVGraph, this can be done using recompress but this is faster.", long_about = None)]
 struct Args {
@@ -85,35 +87,42 @@ struct Args {
 }
 macro_rules! impl_convert {
     ($args:expr, $src:ty, $dst:ty) => {
-        println!("The source graph was {}, converting to {}", <$src>::NAME, <$dst>::NAME);
+        info!(
+            "The source graph was {}-endian, converting to {}-endian",
+            <$src>::NAME,
+            <$dst>::NAME
+        );
 
-        let (num_nodes, num_arcs, comp_flags) = parse_properties::<$src>(format!("{}.properties", $args.src_basename))?;
+        let (num_nodes, num_arcs, comp_flags) =
+            parse_properties::<$src>(format!("{}.properties", $args.src_basename))?;
         let mut pl = ProgressLogger::default();
         pl.display_memory(true)
-            .item_name("arcs")
+            .item_name("node")
             .expected_updates(Some(num_arcs as usize));
-        pl.start("Converting graph");
 
         let seq_graph = BVGraphSeq::with_basename(&$args.src_basename)
-                .endianness::<$src>()
-                .load()?;
+            .endianness::<$src>()
+            .load()?;
         // build the encoder with the opposite endianness
-        std::fs::write(format!("{}.properties", $args.dst_basename), comp_flags.to_properties::<$dst>(num_nodes, num_arcs)?)?;
-        let writer = <BufBitWriter<$dst, _>>::new(<WordAdapter<usize, _>>::new(
-            BufWriter::new(File::create(&format!("{}.graph", $args.dst_basename))?),
-        ));
+        std::fs::write(
+            format!("{}.properties", $args.dst_basename),
+            comp_flags.to_properties::<$dst>(num_nodes, num_arcs)?,
+        )?;
+        let writer = <BufBitWriter<$dst, _>>::new(<WordAdapter<usize, _>>::new(BufWriter::new(
+            File::create(&format!("{}.graph", $args.dst_basename))?,
+        )));
         let encoder = <DynCodesEncoder<$dst, _>>::new(writer, &comp_flags);
         // build the iterator that will read the graph and write it to the encoder
-        let mut iter = seq_graph.offset_deg_iter()
-            .map_decoder(move |decoder| {
-                Converter {
-                    decoder,
-                    encoder,
-                }
-        });
+
+        pl.start("Inverting endianness...");
+
+        let mut iter = seq_graph
+            .offset_deg_iter()
+            .map_decoder(move |decoder| Converter { decoder, encoder });
         // consume the graph iterator reading all codes, but do nothing with them
         for _ in 0..num_nodes {
-            pl.update_with_count(iter.next_degree()?);
+            iter.next_degree()?;
+            pl.light_update();
         }
         pl.done();
     };
