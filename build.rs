ca6933a453bef1bcdcd5c45645adfbc812292872
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2025-02-26 17:59:28 +0100

    added build date without time to build-info

--- build.rs
+++ build.rs
@@ -1,3 +1,16 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
 fn main() {
-    built::write_built_file().expect("Failed to acquire build-time information")
+    built::write_built_file().expect("Failed to acquire build-time information");
+    let dst = std::path::Path::new(&std::env::var("OUT_DIR").expect("OUT_DIR not set")).join("built.rs");
+    
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(dst).expect("Could not open file written by built");
+    
+    // write the date in DST
+    writeln!(file, "pub const BUILD_DATE: &str = \"{}\";\n", chrono::Utc::now().format("%Y-%m-%d"))
+        .expect("Failed to write build date");
 }
