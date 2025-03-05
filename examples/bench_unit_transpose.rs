f303f2c88fb7837e04d5675838e8b1d23aec04d4
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-03-05 17:10:39 +0000

    Types ok in load, but endianness not work

--- examples/bench_unit_transpose.rs
+++ examples/bench_unit_transpose.rs
@@ -67,7 +67,7 @@ pub fn transpose(
 
 fn bench_impl<E: Endianness + 'static>(args: Args) -> Result<()>
 where
-    for<'a> BufBitReader<E, MemWordReader<u32, &'a [u32]>>: CodeRead<E> + BitSeek,
+    for<'a> BufBitReader<E, MemWordReader<u32, &'a [u32]>>: CodesRead<E> + BitSeek,
 {
     let graph = webgraph::graphs::bvgraph::sequential::BvGraphSeq::with_basename(args.basename)
         .endianness::<E>()
