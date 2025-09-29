f1ded7abcb5d4ebabf79657107846cfd507d06d4
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-09-29 15:38:25 +0200

    Fixed test for new ArcListGraph bounds

--- webgraph/tests/test_arc_list_graph.rs
+++ webgraph/tests/test_arc_list_graph.rs
@@ -13,7 +13,8 @@ use webgraph::{
 
 #[test]
 fn test_arc_list_graph_iter() {
-    let iter = Iter::<Box<u64>, Vec<_>>::new(10, vec![].into_iter());
+    let iter =
+        Iter::<Box<u64>, std::vec::IntoIter<(usize, usize, Box<u64>)>>::new(10, vec![].into_iter());
     for_!((_succ, labels) in iter {
         for_!(item in labels {
           println!("{:?}", item);
@@ -74,14 +75,14 @@ fn test_arc_list_graph_cnr2000() {
     });
     assert_eq!(arcs.len(), graph.num_arcs() as _);
 
-    let arcgraph =
+    let arc_graph =
         webgraph::graphs::arc_list_graph::ArcListGraph::new(graph.num_nodes(), arcs.into_iter());
 
-    assert_eq!(arcgraph.num_nodes(), graph.num_nodes());
-    test_graph_iters(arcgraph.iter(), graph.iter());
+    assert_eq!(arc_graph.num_nodes(), graph.num_nodes());
+    test_graph_iters(arc_graph.iter(), graph.iter());
 
     for n in 1..=11 {
-        let iters = arcgraph.split_iter(n);
+        let iters = arc_graph.split_iter(n);
         let truth_iters = graph.split_iter(n);
 
         assert_eq!(truth_iters.len(), n, "Expected {} iterators", n);
